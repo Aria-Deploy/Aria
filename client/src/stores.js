7ce097d3d7b7ad35486b9f97ca37ceec90ed8b3c
SamGrah <segrah@gmail.com>
2021-11-17 15:23:41 -0500

    removed test UI, server to root

--- client/src/stores.js
+++
@@ -1,6 +0,0 @@
-import { writable } from "svelte/store";
-
-export const userProfiles = writable();
-export const selectedProfile = writable();
-export const profileStacks = writable([]);
-export const profileResourceData = writable([]);
