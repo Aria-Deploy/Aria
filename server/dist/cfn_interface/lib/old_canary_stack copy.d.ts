f147bd7b4f42d2d6eab626d847133b1ff2bb526d
SamGrah <segrah@gmail.com>
2021-11-19 19:55:38 -0500

    removed TS dist folder

--- server/dist/cfn_interface/lib/old_canary_stack copy.d.ts
+++
@@ -1,5 +0,0 @@
-import * as cdk from "@aws-cdk/core";
-import { ExistingStack } from "./existing_stack";
-export declare class CanaryStack extends ExistingStack {
-    constructor(id: string, stackConfig: any, props?: cdk.StackProps);
-}
--- server/dist/cfn_interface/lib/old_canary_stack.d.ts
+++
@@ -1,5 +0,0 @@
-import * as cdk from "@aws-cdk/core";
-import { ExistingStack } from "./existing_stack";
-export declare class CanaryStack extends ExistingStack {
-    constructor(source: cdk.App, id: string, stackConfig: any, props?: cdk.StackProps);
-}
