c4b735e93caba48b22d3956d93dc76f08c8af12e
SamGrah <segrah@gmail.com>
2021-11-21 16:32:23 -0500

    added sidebar MenuItems

---
+++ client/src/lib/sidebar/sidebar.svelte
@@ -0,0 +1,53 @@
+<script>
+  import XxXsidebar from "./XXXsidebar.svelte";
+  import MenuItem from "./menu-item.svelte";
+
+</script>
+
+<div
+  class="fixed flex flex-col bg-regal-blue items-center h-full w-48 p-5 text-blue-100"
+>
+  <img class="w-9/12 h-auto pb-10 pt-6" src="duck.png" alt="" />
+  <div class="space-y-2">
+    <MenuItem title={"Deployments"} route={"/"} >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        />    
+    </MenuItem>
+    <MenuItem title={"Status"} route={"/status"} >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        />    
+    </MenuItem>
+    <MenuItem title={"Deploy Demo"} route={"/"} >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        />    
+    </MenuItem>
+    <MenuItem title={"Instructions"} route={"/"} >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        />    
+    </MenuItem>
+    <MenuItem title={"About"} route={"/"} >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        />    
+    </MenuItem>
+  </div>
+</div>
