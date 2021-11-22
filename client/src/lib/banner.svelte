303a1cc9602a64f91cfb8f68d4382db10412c76d
Yue <yue@yues-air.lan>
2021-11-22 15:02:15 -0800

    updated create canary form

--- client/src/lib/banner.svelte
+++ client/src/lib/banner.svelte
@@ -1,77 +1,12 @@
 <script>
-  import Modal from "$lib/modal.svelte"
+  import Modal from "$lib/modal.svelte";
+  import CanaryForm from "$lib/create_canary_form.svelte";
 
-  let showModal = false
+  let showModal = false;
 
   const handleToggleModal = () => {
-    showModal = !showModal
-  }
-
-  function selectAlb(event) {
-		// selectedAlb = { ...$profileResourceData[event.target.value] };
-	}
-
-	function selectListener(event) {
-		// selectedListener = { ...selectedAlb.Listeners[event.target.value] };
-	}
-
-	function selectTargetGroup(event) {
-		// selectedTarget = { ...selectedAlb.Targets[event.target.value] };
-	}
-
-	function selectInstance(event) {
-		// selectedInstance = { ...selectedTarget.Instances[event.target.value] };
-		// deployDisabled = false;
-	}
-  
-	async function deployCanary() {
-		const apiURI = `${apiRoute}/deploy-canary`;
-		const securityGroupIds = selectedInstance.SecurityGroups.map((sg) => ({
-			groupId: sg.GroupId
-		}));
-		const newRuleConfig = {
-			Actions: [
-				{
-					Type: 'forward',
-					ForwardConfig: {
-						TargetGroupStickinessConfig: {
-							Enabled: false
-						},
-						TargetGroups: [
-							{
-								TargetGroupArn: selectedTarget.TargetGroupArn,
-								Weight: 50
-							},
-							{
-								TargetGroupArn: 'Insert Canary Target ARN',
-								Weight: 50
-							}
-						]
-					}
-				}
-			],
-			Conditions: [
-				{
-					Field: 'http-request-method',
-					HttpRequestMethodConfig: {
-						Values: ['GET']
-					}
-				}
-			],
-			ListenerArn: selectedListener.ListenerArn,
-			Priority: 1,
-			Tags: [{ Key: 'isAriaCanaryRule', Value: selectedAlb.LoadBalancerName }]
-		};
-		const response = await axios.put(apiURI, {
-			profileName,
-			vpcId: selectedInstance.VpcId,
-			selectedAlbName: selectedAlb.LoadBalancerName,
-			selectedListenerArn: selectedListener.ListenerArn,
-			securityGroupIds,
-			newRuleConfig
-		});
-		console.log(response.data);
-	}
+    showModal = !showModal;
+  };
 </script>
 
 <div class="bg-blue-100 flex flex-row px-9 py-12 shadow">
@@ -83,14 +18,17 @@
   </div>
 
   <div class="flex-shrink">
-    <button on:click={() => handleToggleModal()} class="py-2 px-4 bg-blue-600 text-white rounded-md shadow-xl hover:shadow-2xl hover:bg-blue-700 transition duration-500">
+    <button
+      on:click={() => handleToggleModal()}
+      class="py-2 px-4 bg-blue-600 text-white rounded-md shadow-xl hover:shadow-2xl hover:bg-blue-700 transition duration-500"
+    >
       Create New
     </button>
   </div>
 </div>
 
-<Modal title="Edit your details" open={showModal} on:close={() => handleToggleModal()}>
+<Modal open={showModal} on:close={() => handleToggleModal()}>
   <svelte:fragment slot="body">
-    This is content inside my modal! 👋
+    <CanaryForm />
   </svelte:fragment>
 </Modal>
